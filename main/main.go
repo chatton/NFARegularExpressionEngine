a40af8a3641d5afe14fa67443d4ae23e3632f916
chatton <cianhatton@gmail.com>
2018-02-20 17:09:46 +0000

    implemented shunting yard algorithm.

--- main/main.go
+++ main/main.go
@@ -3,20 +3,51 @@ package main
 import "fmt"
 import "../stack"
 
-func infixToPostfix(regex string) string {
-	return ""
+func infixToPostfix(infix string) string {
+
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
+	postfix := stack.New()
+	stack := stack.New()
+
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			stack.Push(r)
+		case r == ')':
+			for stack.Peek() != '(' {
+				postfix.Push(stack.Pop())
+			}
+			stack.Pop()
+
+		case specials[r] > 0:
+			for !stack.IsEmpty() && specials[r] <= specials[stack.Peek()] {
+				postfix.Push(stack.Pop())
+			}
+			stack.Push(r)
+		default:
+			postfix.Push(r)
+		}
+	}
+
+	for !stack.IsEmpty() {
+		postfix.Push(stack.Pop())
+	}
+
+	result := postfix.Data()
+	return string(result)
 }
 
 func main() {
 
-	s := stack.New()
-	s.Push("1")
-	s.Push("2")
-	s.Push("3")
-	s.Push("4")
-	s.Push("5")
+	fmt.Println("Infix:    ", "a.b.c*")
+	fmt.Println("Postfix:  ", infixToPostfix("a.b.c*"))
 
-	for i := 0; i < 5; i++ {
-		fmt.Println(s.Pop())
-	}
+	fmt.Println("Infix:    ", "(a.(b|d))*")
+	fmt.Println("Postfix:  ", infixToPostfix("(a.(b|d))*"))
+
+	fmt.Println("Infix:    ", "a.(b|d).c*")
+	fmt.Println("Postfix:  ", infixToPostfix("a.(b|d).c*"))
+
+	fmt.Println("Infix:    ", "a.(b.b)+.c")
+	fmt.Println("Postfix:  ", infixToPostfix("a.(b.b)+.c"))
 }
--- stack/stack.go
+++ stack/stack.go
@@ -1,13 +1,21 @@
 package stack
 
-import "errors"
-
 func New() *Stack {
 	return &Stack{}
 }
 
 type Stack struct {
-	data []string
+	data []rune
+}
+
+func (s *Stack) Data() []rune {
+	tmp := make([]rune, len(s.data))
+	copy(tmp, s.data)
+	return tmp
+}
+
+func (s *Stack) Peek() rune {
+	return s.data[len(s.data)-1]
 }
 
 func (s *Stack) IsEmpty() bool {
@@ -18,22 +26,20 @@ func (s *Stack) Size() int {
 	return len(s.data)
 }
 
-func (s *Stack) Push(elem string) {
+func (s *Stack) Push(elem rune) {
 	s.data = append(s.data, elem)
 }
 
-func (s *Stack) Pop() (string, error) {
-	if s.IsEmpty() {
-		return "", errors.New("stack was empty")
-	}
+func (s *Stack) Pop() rune {
+
 	lastIndex := len(s.data) - 1
 	lastItem := s.data[lastIndex]
 	s.data = remove(s.data, lastIndex)
-	return lastItem, nil
+	return lastItem
 }
 
 // this SO post had an answer for how to remove an element from a slice while maintaining order.
 // https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-array-in-golang/37335777
-func remove(slice []string, s int) []string {
+func remove(slice []rune, s int) []rune {
 	return append(slice[:s], slice[s+1:]...)
 }

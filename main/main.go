3e5a34aeba9ec2156d95a65efd8d3e3b9eeb6a06
chatton <cianhatton@gmail.com>
2018-03-20 01:53:05 +0000

    Matching function working with tokens.

--- main/main.go
+++ main/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"../nfa"
+	"fmt"
 	//"fmt"
 )
 
 func main() {
 	//n := nfa.Compile("b.(c+).(e?)")
 	//fmt.Println(n.Matches("bccccccc"))
-	nfa.Tokenize(`\d+e`)
-	nfa.InfixToPostfix("a.(b|c)")
+	//nfa.Tokenize(`\d+e`)
+	//nfa.InfixToPostfix("a.(b|c)")
+	n := nfa.Compile("(a|b+)")
+	fmt.Println(n.Matches("bbbbbbbbbb"))
 }
--- nfa/nfa.go
+++ nfa/nfa.go
@@ -2,27 +2,21 @@ package nfa
 
 import (
 	"github.com/golang-collections/collections/stack" // useful stack data structure
-	"strings"
 	"unicode"
 )
 
 type nfa struct {
 	Initial, Accept *State
-	postfix         string
+	tokens          []Token
 }
 
 // provide mechanism for creating compiled Nfas so you don't need to construct the nfa each time you use it.
 func Compile(infix string) *nfa {
-	postfix := InfixToPostfix(infix)
-	n := postFixToNfa(postfix)
-	n.postfix = postfix
+	tokens := InfixToPostfix(infix)
+	n := tokensToNfa(tokens)
+	n.tokens = tokens
 	return n
 }
-
-// use the saved nfa to match the provided string
-//func (n *nfa) Matches(matchString string) bool {
-//	return postfixMatch(n.postfix, matchString)
-//}
 
 type State struct {
 	symbol       interface{}
@@ -74,12 +68,11 @@ func (t DigitToken) Val() string {
 	return t.val
 }
 
-func postFixToNfa(postfix string) *nfa {
-
+func tokensToNfa(tokens []Token) *nfa {
 	nfaStack := stack.New()
-	for _, r := range postfix {
-		switch r {
-		case '.':
+	for _, tok := range tokens {
+		switch tok.(Token).Val() {
+		case ".":
 			// take 2 elements off the stack
 			frag2 := nfaStack.Pop().(*nfa)
 			frag1 := nfaStack.Pop().(*nfa)
@@ -87,7 +80,7 @@ func postFixToNfa(postfix string) *nfa {
 			frag1.Accept.edge1 = frag2.Initial
 			// place the single fragment on top of the stack
 			nfaStack.Push(&nfa{Initial: frag1.Initial, Accept: frag2.Accept})
-		case '|':
+		case "|":
 			// take 2 elements off the stack
 			frag2 := nfaStack.Pop().(*nfa)
 			frag1 := nfaStack.Pop().(*nfa)
@@ -100,7 +93,7 @@ func postFixToNfa(postfix string) *nfa {
 
 			// add the new fragment to the stack.
 			nfaStack.Push(&nfa{Initial: &initial, Accept: &accept})
-		case '*':
+		case "*":
 			// take a single element off of the stack
 			frag := nfaStack.Pop().(*nfa)
 			accept := State{}
@@ -112,7 +105,7 @@ func postFixToNfa(postfix string) *nfa {
 			frag.Accept.edge2 = &accept
 			// add it to the stack
 			nfaStack.Push(&nfa{Initial: &initial, Accept: &accept})
-		case '+':
+		case "+":
 			// take a single element off of the stack
 			frag := nfaStack.Pop().(*nfa)
 			accept := State{}
@@ -122,7 +115,7 @@ func postFixToNfa(postfix string) *nfa {
 			frag.Accept.edge2 = nil
 
 			nfaStack.Push(&nfa{Initial: frag.Initial, Accept: &accept})
-		case '?':
+		case "?":
 			// take a single element off of the stack
 			frag := nfaStack.Pop().(*nfa)
 			// create a new state that points to the existing item and also the accept state
@@ -131,17 +124,93 @@ func postFixToNfa(postfix string) *nfa {
 			nfaStack.Push(&nfa{Initial: &initial, Accept: frag.Accept})
 		default:
 			accept := State{}
-			initial := State{symbol: CharacterClassToken{string(r)}, edge1: &accept}
+			initial := State{edge1: &accept, symbol: tok}
+
 			nfaStack.Push(&nfa{Initial: &initial, Accept: &accept})
+
 		}
 	}
 
-	if nfaStack.Len() != 1 {
-		panic("Nfa stack didn't have just a single element in it.")
-	}
-	res := nfaStack.Pop().(*nfa)
-	return res
+	return nfaStack.Pop().(*nfa)
 }
+
+//func postFixToNfa(postfix string) *nfa {
+//
+//	nfaStack := stack.New()
+//	for _, r := range postfix {
+//		switch r {
+//		case '.':
+//			// take 2 elements off the stack
+//			frag2 := nfaStack.Pop().(*nfa)
+//			frag1 := nfaStack.Pop().(*nfa)
+//			// join them together
+//			frag1.Accept.edge1 = frag2.Initial
+//			// place the single fragment on top of the stack
+//			nfaStack.Push(&nfa{Initial: frag1.Initial, Accept: frag2.Accept})
+//		case '|':
+//			// take 2 elements off the stack
+//			frag2 := nfaStack.Pop().(*nfa)
+//			frag1 := nfaStack.Pop().(*nfa)
+//			// create a new initial state which points at both fragments
+//			initial := State{edge1: frag1.Initial, edge2: frag2.Initial}
+//			accept := State{}
+//			// point both fragments at the new accept state
+//			frag1.Accept.edge1 = &accept
+//			frag2.Accept.edge1 = &accept
+//
+//			// add the new fragment to the stack.
+//			nfaStack.Push(&nfa{Initial: &initial, Accept: &accept})
+//		case '*':
+//			// take a single element off of the stack
+//			frag := nfaStack.Pop().(*nfa)
+//			accept := State{}
+//			// create and edge pointing back at itself and one towards the new accept state
+//			initial := State{edge1: frag.Initial, edge2: &accept}
+//			// the accept state points back at the initial state
+//			frag.Accept.edge1 = frag.Initial
+//			// the second edge loops around
+//			frag.Accept.edge2 = &accept
+//			// add it to the stack
+//			nfaStack.Push(&nfa{Initial: &initial, Accept: &accept})
+//		case '+':
+//			// take a single element off of the stack
+//			frag := nfaStack.Pop().(*nfa)
+//			accept := State{}
+//			initial := State{edge1: frag.Initial, edge2: &accept}
+//
+//			frag.Accept.edge1 = &initial
+//			frag.Accept.edge2 = nil
+//
+//			nfaStack.Push(&nfa{Initial: frag.Initial, Accept: &accept})
+//		case '?':
+//			// take a single element off of the stack
+//			frag := nfaStack.Pop().(*nfa)
+//			// create a new state that points to the existing item and also the accept state
+//			initial := State{edge1: frag.Initial, edge2: frag.Accept}
+//			// push the new Nfa onto the stack
+//			nfaStack.Push(&nfa{Initial: &initial, Accept: frag.Accept})
+//		default:
+//			accept := State{}
+//			initial := State{edge1: &accept}
+//			switch r {
+//			case 'd':
+//				initial.symbol = DigitToken{string(r)}
+//			case 'w':
+//				initial.symbol = WordToken{string(r)}
+//			default:
+//				initial.symbol = CharacterClassToken{string(r)}
+//			}
+//
+//			nfaStack.Push(&nfa{Initial: &initial, Accept: &accept})
+//		}
+//	}
+//
+//	if nfaStack.Len() != 1 {
+//		panic("Nfa stack didn't have just a single element in it.")
+//	}
+//	res := nfaStack.Pop().(*nfa)
+//	return res
+//}
 
 func IsEmpty(s *stack.Stack) bool {
 	return s.Len() == 0
@@ -162,9 +231,9 @@ func Tokenize(infix string) []interface{} {
 			wantsToEscape = false
 			switch r {
 			case 'd': // \d
-				tokens = append(tokens, DigitToken{val: string(r)})
+				tokens = append(tokens, DigitToken{val: `\d`})
 			case 'w': // \w
-				tokens = append(tokens, WordToken{val: string(r)})
+				tokens = append(tokens, WordToken{val: `\w`})
 			case '\\': // a second backslash, treat it as a backslash literal
 				tokens = append(tokens, CharacterClassToken{val: string(r)})
 			}
@@ -197,7 +266,7 @@ func Tokenize(infix string) []interface{} {
 	return tokens
 }
 
-func InfixToPostfix(infix string) string {
+func InfixToPostfix(infix string) []Token {
 
 	specials := map[string]int{"*": 10, ".": 9, "+": 8, "|": 7, "?": 6}
 
@@ -230,90 +299,68 @@ func InfixToPostfix(infix string) string {
 		postfix.Push(tempStack.Pop())
 	}
 
-	var result []string
+	var result []Token
 
 	for !IsEmpty(postfix) {
-		result = append([]string{postfix.Pop().(Token).Val()}, result...)
+		// insert every element at the start of the list.
+		result = append([]Token{postfix.Pop().(Token)}, result...)
 	}
 
-	return strings.Join(result, "")
-
-	//for !IsEmpty(tempStack) {
-	//	postfix.Push(tempStack.Pop())
-	//}
-	//
-	//// return the elements as a more generic slice instead of a stack.
-	//// stack is an implementation detail of the algorithm
-	//var result []string
-	//for !IsEmpty(postfix) {
-	//	result = append([]string{postfix.Pop().(string)}, result...)
-	//}
-	//return strings.Join(result, "")
+	return result
 }
 
-// returns all the possible states from the given state, including
-// state transitions with e arrows
-//func addState(possibilities []*State, from, to *State) []*State {
-//	seen := make(map[*State]bool) // keep track of the states we've already visited
-//
-//	states := stack.New()
-//	states.Push(from)
-//
-//	for !IsEmpty(states) { // keep looking until dead end
-//		next := states.Pop().(*State)
-//
-//		if seen[next] { // we may be looking at a state we've already seen
-//			continue
-//		}
-//
-//		// mark the state as having been seen already so we don't examine it again.
-//		seen[next] = true
-//		possibilities = append(possibilities, next) // this is a valid destination
-//
-//		// e arrow if symbol == 0
-//		if next != to && next.symbol == 0 {
-//			states.Push(next.edge1)
-//			if next.edge2 != nil {
-//				states.Push(next.edge2)
-//			}
-//		}
-//	}
-//
-//	return possibilities
-//}
+//returns all the possible states from the given state, including
+//state transitions with e arrows
+func addState(possibilities []*State, from, to *State) []*State {
+	seen := make(map[*State]bool) // keep track of the states we've already visited
 
-//func addState(states []*State, start, accept *State) []*State {
-//	states = append(states, start)
-//	if start != accept && start.symbol == 0 {
-//		states = addState(states, start.edge1, accept)
-//		if start.edge2 != nil {
-//			states = addState(states, start.edge2, accept)
-//		}
-//	}
-//	return states
-//}
-//
-//func postfixMatch(postfix, matchString string) bool {
-//	nfa := postFixToNfa(postfix)
-//
-//	var current []*State
-//	current = addState(current, nfa.Initial, nfa.Accept)
-//	var next []*State
-//
-//	for _, r := range matchString {
-//		for _, curr := range current {
-//			if curr.symbol == r {
-//				next = addState(next[:], curr.edge1, nfa.Accept)
-//			}
-//		}
-//		current, next = next, []*State{}
-//	}
-//
-//	for _, curr := range current {
-//		if curr == nfa.Accept {
-//			return true
-//		}
-//	}
-//
-//	return false
-//}
+	states := stack.New()
+	states.Push(from)
+
+	for !IsEmpty(states) { // keep looking until dead end
+		next := states.Pop().(*State)
+
+		if seen[next] { // we may be looking at a state we've already seen
+			continue
+		}
+
+		// mark the state as having been seen already so we don't examine it again.
+		seen[next] = true
+		possibilities = append(possibilities, next) // this is a valid destination
+
+		// e arrow if symbol == nil
+		// zero value for Token is nil
+		if next != to && next.symbol == nil {
+			states.Push(next.edge1)
+			if next.edge2 != nil {
+				states.Push(next.edge2)
+			}
+		}
+	}
+
+	return possibilities
+}
+
+func (n *nfa) Matches(matchString string) bool {
+
+	var current []*State
+	current = addState(current, n.Initial, n.Accept)
+	var next []*State
+
+	for _, r := range matchString {
+		for _, curr := range current {
+			if curr.symbol != nil && curr.symbol.(Token).Val() == string(r) {
+				next = addState(next[:], curr.edge1, n.Accept)
+			}
+		}
+		current, next = next, []*State{}
+	}
+
+	for _, curr := range current {
+		if curr == n.Accept {
+			return true
+		}
+	}
+
+	return false
+}
